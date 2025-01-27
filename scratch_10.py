bf8eca6c3df43d2f49f73354f5eae20fa3839091
TimGank <tima.gankin@mail.ru>
2025-01-27 14:12:19 +0300

    Погоня кружка за мышкой. Реализация монстра.

--- scratch_10.py
+++ scratch_10.py
@@ -4,21 +4,20 @@ import sys
 # Инициализация Pygame
 pygame.init()
 
-# Установка размеров окна
+# Настройки окна
 width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Погоня красного кружка за мышкой")
+pygame.display.set_caption("Smooth Chasing Circle")
 
 # Цвета
 red = (255, 0, 0)
 background_color = (255, 255, 255)
 
-# Параметры кружка
-circle_radius = 20
-circle_speed = 5
-
-# Начальная позиция кружка
-circle_x, circle_y = width // 2, height // 2
+# Позиция кружка
+circle_pos = [width // 2, height // 2]
+circle_radius = 30
+# Скорость движения круга
+speed = 0.03
 
 # Главный игровой цикл
 while True:
@@ -30,27 +29,18 @@ while True:
     # Получение позиции мыши
     mouse_x, mouse_y = pygame.mouse.get_pos()
 
-    # Проверка, находится ли мышь в пределах экрана
-    if 0 <= mouse_x < width and 0 <= mouse_y < height:
-        # Вычисление направления движения кружка
-        if circle_x < mouse_x:
-            circle_x += circle_speed
-        elif circle_x > mouse_x:
-            circle_x -= circle_speed
-
-        if circle_y < mouse_y:
-            circle_y += circle_speed
-        elif circle_y > mouse_y:
-            circle_y -= circle_speed
+    # Проверка, находится ли мышка в пределах окна
+    if 0 <= mouse_x <= width and 0 <= mouse_y <= height:
+        # Плавное движение круга
+        circle_pos[0] += (mouse_x - circle_pos[0]) * speed
+        circle_pos[1] += (mouse_y - circle_pos[1]) * speed
 
     # Очистка экрана
     screen.fill(background_color)
 
     # Рисование кружка
-    pygame.draw.circle(screen, red, (circle_x, circle_y), circle_radius)
+    pygame.draw.circle(screen, red, (int(circle_pos[0]), int(circle_pos[1])), circle_radius)
 
     # Обновление экрана
     pygame.display.flip()
-
-    # Ограничение кадров в секунду
     pygame.time.Clock().tick(60)
